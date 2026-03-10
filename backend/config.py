1928dc2e72694cf486c0b0e9b74341569d32c985
Sreenath P R <sreenath.pr21@gmail.com>
2026-03-10 22:09:25 +0530

    Updated audio generation logic and bug fixes

--- backend/config.py
+++ backend/config.py
@@ -6,6 +6,6 @@ AWS_REGION = os.getenv("AWS_REGION", "ap-south-1")
 MODEL_CACHE_DIR = os.getenv("MODEL_CACHE_DIR", "./model_cache")
 OUTPUT_AUDIO_DIR = os.getenv("OUTPUT_AUDIO_DIR", "./outputs")
 
-WHISPER_MODEL_SIZE = os.getenv("WHISPER_MODEL_SIZE", "base")
+WHISPER_MODEL_SIZE = os.getenv("WHISPER_MODEL_SIZE", "tiny")
 
 TARGET_LANGUAGE = os.getenv("TARGET_LANGUAGE", "hin_Deva")
